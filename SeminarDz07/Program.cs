a659386e799830a8318149a7904f872e37e23d9d
SergeySeroff <harek20@mail.ru>
2022-07-10 16:30:47 +0300

    add seven example

---
+++ SeminarDz07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.Read());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
